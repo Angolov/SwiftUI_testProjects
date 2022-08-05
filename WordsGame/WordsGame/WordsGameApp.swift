eb23549516d459ba18e3e6dddc50e3eaa801f4bc
Angolov <anton-golovatyy@yandex.ru>
2022-08-05 10:54:18 +0300

    WordsGame project start

---
+++ WordsGame/WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Антон Головатый on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
