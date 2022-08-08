fe663ec8c903e86adc3b89126446ce54067a8164
Angolov <anton-golovatyy@yandex.ru>
2022-08-08 16:34:02 +0300

    feat(FoodDelivery): add Product model

---
+++ FoodDelivery/FoodDelivery/Model/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  FoodDelivery
+//
+//  Created by Антон Головатый on 08.08.2022.
+//
+
+import Foundation
+
+struct Product {
+    var id: String
+    var title: String
+    var imageUrl: String
+    var price: Int
+    var description: String
+    
+//    var ordersCount: Int
+//    var isRecommended: Bool
+}
