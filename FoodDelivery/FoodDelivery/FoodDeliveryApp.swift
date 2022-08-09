4e6c42dea5ed2d2523864a6f6888bbdacedb6182
Angolov <anton-golovatyy@yandex.ru>
2022-08-09 21:07:28 +0300

    feat(FoodDelivery): add FireBase support

--- FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
+++ FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
@@ -6,11 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 let screen = UIScreen.main.bounds
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct FoodDeliveryApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             AuthView()
