1eed12405d1753491f7cc96b856b611e4ad40cd2
Angolov <anton-golovatyy@yandex.ru>
2022-08-13 20:42:18 +0300

    feat(FoodDelivery): add SessionManager service

---
+++ FoodDelivery/FoodDelivery/Services/SessionManager.swift
@@ -0,0 +1,16 @@
+//
+//  SessionManager.swift
+//  FoodDelivery
+//
+//  Created by Антон Головатый on 13.08.2022.
+//
+
+import Foundation
+
+class SessionManager {
+    
+    static let shared = SessionManager()
+    private init() {}
+    
+    var userID = ""
+}
